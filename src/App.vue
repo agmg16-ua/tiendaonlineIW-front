8ea583144e7d660cce4dcf557783305618ee5fa0
agmg16-ua <agmg16@alu.ua.es>
2024-12-15 18:19:40 +0100

    Creada barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-title>L E W K I N</ion-title>
+          <ion-button>
+            <Router-Link to="/">Inicio</Router-Link>
+          </ion-button>
+          <ion-button>
+            <Router-Link to="/about">Nosotros</Router-Link>
+          </ion-button>
+          <ion-button>
+            <Router-Link to="/contact">Contacta con Nosotros</Router-Link>
+          </ion-button>
+        </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-button>Carrito</ion-button>
+          <ion-button>Iniciar Sesión</ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <RouterView/>
+    </ion-content>
+
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title>© 2021 L E W K I N</ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-app>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+  import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
