d72a404878717b4297075cb53893d0928890992b
Vahe <vmm111@alu.ua.es>
2025-01-01 21:22:05 +0100

    Añadido filtrado por material y refactorizado codigo del CatalogPage

--- src/views/CatalogPage.vue
+++ src/views/CatalogPage.vue
@@ -1,65 +1,81 @@
 <template>
-  <div>
-    <!-- Filtro de precio -->
-    <div class="filter-container">
-      <IonButton @click="mostrarFiltro = !mostrarFiltro" size="small" class="filter-button">
+ 
+  <!-- Filtro de precio -->
+  <div class="filter-container">
+    <IonButton @click="mostrarFiltro = !mostrarFiltro" size="small" class="filter-button">
+      <IonIcon :icon="funnelOutline" slot="start" />
+      Precio
+    </IonButton>
+    <div v-if="mostrarFiltro" class="range-container">
+      <IonRange dualKnobs :min="0" :max="200" :step="1" color="primary" v-model="precioRango">
+        <div slot="start">€{{ precioRango.lower }}</div>
+        <div slot="end">€{{ precioRango.upper }}</div>
+      </IonRange>
+    </div>
+    <div class="material-container">
+      <IonButton @click="mostrarMaterialFiltro = !mostrarMaterialFiltro" size="small" class="filter-button">
         <IonIcon :icon="funnelOutline" slot="start" />
-        Precio
+        Material
       </IonButton>
-      <div v-if="mostrarFiltro" class="range-container">
-        <IonRange dualKnobs :min="0" :max="200" :step="1" color="primary" v-model="precioRango" @ionChange="filtrarPorPrecio">
-          <div slot="start">€{{ precioRango.lower }}</div>
-          <div slot="end">€{{ precioRango.upper }}</div>
-        </IonRange>
-      </div>
-      <!-- Botón para ordenar por precio -->
-      <div class="ordenar-container">
-        <IonButton @click="mostrarOrden = !mostrarOrden" size="small" class="filter-button">
-          Ordenar por Precio
+      <div v-if="mostrarMaterialFiltro" class="material-options">
+        <IonButton v-for="material in materialesDisponibles" :key="material.id" size="small" @click="filtrarPorMaterial(material.id)" :class="['material-option-button', { 'active': materialSeleccionado === material.id }]"color="transparent">
+          {{ material.nombre }}
         </IonButton>
-        <!-- Opciones para ordenar -->
-        <div v-if="mostrarOrden" class="order-options">
-          <IonButton size="small" @click="ordenarPorPrecio('asc')" :class="['order-option-button', { 'active': ordenActual === 'asc' }]" color="transparent">
-            Ascendente
-          </IonButton>
-          <IonButton size="small" @click="ordenarPorPrecio('desc')" :class="['order-option-button', { 'active': ordenActual === 'desc' }]" color="transparent">
-            Descendente
-          </IonButton>
-          <!-- Botón para limpiar la ordenación -->
-          <IonButton size="small" @click="limpiarOrdenacion" :class="['order-option-button', { 'active': ordenActual === null }]" color="transparent">
-            Ninguno
-          </IonButton>
-        </div>
+        <IonButton size="small" @click="limpiarFiltroMaterial" :class="['material-option-button', { 'active': materialSeleccionado === null }]" color="transparent">
+          Ninguno
+        </IonButton>
       </div>
     </div>
-    <!-- Verifica si hay productos -->
-    <IonGrid>
-      <IonRow>
-        <IonCol size="12" size-md="4" size-lg="2" v-for="producto in productosFiltrados" :key="producto.id">
-          <IonCard class="product-card" @click="$router.push({ name: 'DetallesProducto', params: { id: producto.id } })">
-            <!-- Imagen del producto en la parte superior -->
-            <Img v-if="producto.foto_portada" :src="producto.foto_portada"  alt="Foto del producto" class="product-image-container"/>
-            <!-- Imagen de ejemplo si no hay foto disponible -->
-            <IonImg v-else src="https://via.placeholder.com/80x80?text=Producto" alt="Foto de ejemplo" class="product-image"/>
-            <IonCardContent>
-              <!-- Nombre del producto debajo de la imagen -->
-              <IonText>
-                <p class="product-name">{{ producto.nombre }}</p>
-              </IonText>
-              <!-- Precio debajo del nombre -->
-              <IonText>
-                <p><strong>Precio:</strong> €{{ producto.precio.toFixed(2) }}</p>
-              </IonText>
-            </IonCardContent>
-          </IonCard>
-        </IonCol>
-      </IonRow>
-    </IonGrid>
-    <!-- Mostrar mensaje si no hay productos -->
-    <div v-if="productosFiltrados.length === 0">
-      <p>No se encontraron productos en el rango seleccionado.</p>
+
+    <!-- Botón para ordenar por precio -->
+    <div class="ordenar-container">
+      <IonButton @click="mostrarOrden = !mostrarOrden" size="small" class="filter-button">
+        Ordenar por Precio
+      </IonButton>
+      <!-- Opciones para ordenar -->
+      <div v-if="mostrarOrden" class="order-options">
+        <IonButton size="small" @click="ordenarPorPrecio('asc')" :class="['order-option-button', { 'active': ordenActual === 'asc' }]" color="transparent">
+          Ascendente
+        </IonButton>
+        <IonButton size="small" @click="ordenarPorPrecio('desc')" :class="['order-option-button', { 'active': ordenActual === 'desc' }]" color="transparent">
+          Descendente
+        </IonButton>
+        <!-- Botón para limpiar la ordenación -->
+        <IonButton size="small" @click="limpiarOrdenacion" :class="['order-option-button', { 'active': ordenActual === null }]" color="transparent">
+          Ninguno
+        </IonButton>
+      </div>
     </div>
   </div>
+  <!-- Verifica si hay productos -->
+  <IonGrid>
+    <IonRow>
+      <IonCol size="12" size-md="4" size-lg="2" v-for="producto in productosFiltrados" :key="producto.id">
+        <IonCard class="product-card" @click="$router.push({ name: 'DetallesProducto', params: { id: producto.id } })">
+          <!-- Imagen del producto en la parte superior -->
+          <Img v-if="producto.foto_portada" :src="producto.foto_portada"  alt="Foto del producto" class="product-image-container"/>
+          <!-- Imagen de ejemplo si no hay foto disponible -->
+          <IonImg v-else src="https://via.placeholder.com/80x80?text=Producto" alt="Foto de ejemplo" class="product-image"/>
+          <IonCardContent>
+            <!-- Nombre del producto debajo de la imagen -->
+            <IonText>
+              <p class="product-name">{{ producto.nombre }}</p>
+              <p class="product-name">{{ producto.idMaterial }}</p>
+            </IonText>
+            <!-- Precio debajo del nombre -->
+            <IonText>
+              <p><strong>Precio:</strong> €{{ producto.precio.toFixed(2) }}</p>
+            </IonText>
+          </IonCardContent>
+        </IonCard>
+      </IonCol>
+    </IonRow>
+  </IonGrid>
+  <!-- Mostrar mensaje si no hay productos -->
+  <div v-if="productosFiltrados.length === 0">
+    <p>No se encontraron productos en el rango seleccionado.</p>
+  </div>
+  
 </template>
 
 <script setup lang="ts">
@@ -89,90 +105,110 @@ const productosFiltrados = ref<ProductoData[]>([]);
 const mostrarFiltro = ref(false);
 const mostrarOrden = ref(false);
 const precioRango = ref<{ lower: number; upper: number }>({ lower: 0, upper: 200 });
-const ordenActual = ref<'asc' | 'desc' | null>(null); // Almacena el orden actual
+const ordenActual = ref<'asc' | 'desc' | null>(null);
+const mostrarMaterialFiltro = ref(false);
+const materialesDisponibles = ref([
+  { id: 1, nombre: 'Algodón' },
+  { id: 2, nombre: 'Lana' },
+  { id: 3, nombre: 'Seda' },
+  { id: 4, nombre: 'Poliéster' },
+]);
+const materialSeleccionado = ref<number | null>(null);
 
 // Función para obtener los productos
-const fetchProductosHotel = async () => {
+const fetchProductos = async () => {
   try {
     const response = await axios.get<ProductoData[]>('http://localhost:8080/lewkin/api/productos');
     productosHotel.value = response.data;
-
-    // Aplicar los filtros y ordenación después de cargar los productos
-    aplicarFiltrosYOrdenacion(); 
+    aplicarFiltros(); // Aplicar los filtros después de cargar los productos
   } catch (error) {
     console.error('Error al obtener los productos del hotel:', error);
   }
 };
 
-// Función para aplicar el filtro de precio
-const filtrarPorPrecio = () => {
-  productosFiltrados.value = productosHotel.value.filter(
-    (producto) => producto.precio >= precioRango.value.lower && producto.precio <= precioRango.value.upper
-  );
-  const savedOrdenActual = localStorage.getItem('ordenActual');
-  
-  if (savedOrdenActual) {
-    if (savedOrdenActual === 'null') {
-      ordenActual.value = null; // Si el valor es 'null', se debe restablecer a 'ninguno'
-    } else {
-      ordenActual.value = savedOrdenActual as 'asc' | 'desc'; // Aplicar orden guardado
-    }
-    // Ejecutar la ordenación de acuerdo con el valor de ordenActual
-    if (ordenActual.value) {
-      ordenarPorPrecio(ordenActual.value);
-    }
+// Función para aplicar todos los filtros acumulativamente
+const aplicarFiltros = () => {
+  let productos = [...productosHotel.value];
+
+  // Aplicar filtro de material si está seleccionado
+  if (materialSeleccionado.value !== null) {
+    productos = productos.filter(producto => producto.idMaterial === materialSeleccionado.value);
   }
 
+  // Aplicar filtro de precio
+  productos = productos.filter(producto => 
+    producto.precio >= precioRango.value.lower && producto.precio <= precioRango.value.upper
+  );
+
+  // Aplicar ordenación si está seleccionada
+  if (ordenActual.value) {
+    productos.sort((a, b) => ordenActual.value === 'asc' ? a.precio - b.precio : b.precio - a.precio);
+  }
+
+  productosFiltrados.value = productos;
+};
+
+// Función para filtrar por material
+const filtrarPorMaterial = (idMaterial: number) => {
+  materialSeleccionado.value = idMaterial;
+  localStorage.setItem('materialSeleccionado', idMaterial.toString()); // Guardar en localStorage
+  aplicarFiltros(); // Aplicar filtros acumulativos
+};
+
+// Función para limpiar el filtro de material
+const limpiarFiltroMaterial = () => {
+  materialSeleccionado.value = null;
+  localStorage.setItem('materialSeleccionado', 'null'); // Guardar en localStorage
+  aplicarFiltros(); // Aplicar filtros acumulativos
 };
 
 // Función para ordenar los productos por precio
 const ordenarPorPrecio = (orden: 'asc' | 'desc') => {
-  ordenActual.value = orden; // Guardar el orden actual
-  productosFiltrados.value.sort((a, b) => {
-    return orden === 'asc' ? a.precio - b.precio : b.precio - a.precio;
-  });
+  ordenActual.value = orden;
+  localStorage.setItem('ordenActual', orden); // Guardar en localStorage
+  aplicarFiltros(); // Aplicar filtros acumulativos
 };
 
-// Función para limpiar la ordenación (poner 'Ninguno')
+// Función para limpiar la ordenación
 const limpiarOrdenacion = () => {
-  ordenActual.value = null; // Restablecer la ordenación a 'ninguno'
-  productosFiltrados.value = [...productosHotel.value]; // Recargar los productos sin ordenar
-  localStorage.setItem('ordenActual', 'null');
-  filtrarPorPrecio();
+  ordenActual.value = null;
+  localStorage.setItem('ordenActual', 'null'); // Guardar en localStorage
+  aplicarFiltros(); // Aplicar filtros acumulativos
 };
 
-// Función para aplicar los filtros y ordenación guardados
-const aplicarFiltrosYOrdenacion = () => {
-  // Recuperar el rango de precio y orden guardados en localStorage
+// Función para inicializar filtros desde localStorage
+const aplicarFiltrosDesdeLocalStorage = () => {
   const savedPrecioRango = localStorage.getItem('precioRango');
-
   if (savedPrecioRango) {
-    precioRango.value = JSON.parse(savedPrecioRango); // Aplicar rango de precio guardado
-    filtrarPorPrecio();
-  } else {
-    productosFiltrados.value = [...productosHotel.value]; // Copiar todos los productos
+    precioRango.value = JSON.parse(savedPrecioRango);
   }
+
+  const savedOrdenActual = localStorage.getItem('ordenActual');
+  if (savedOrdenActual) {
+    ordenActual.value = savedOrdenActual === 'null' ? null : savedOrdenActual as 'asc' | 'desc';
+  }
+
+  const savedMaterialSeleccionado = localStorage.getItem('materialSeleccionado');
+  if (savedMaterialSeleccionado) {
+    materialSeleccionado.value = savedMaterialSeleccionado === 'null' ? null : parseInt(savedMaterialSeleccionado, 10);
+  }
+
+  aplicarFiltros(); // Aplicar todos los filtros acumulativamente
 };
 
-// Guardar el estado de los filtros y ordenación en localStorage
-watch(precioRango, (newVal) => {
-  localStorage.setItem('precioRango', JSON.stringify(newVal));
-  filtrarPorPrecio();
+// Guardar el rango de precios en localStorage y aplicar filtros
+watch(precioRango, () => {
+  localStorage.setItem('precioRango', JSON.stringify(precioRango.value));
+  aplicarFiltros();
 });
 
-watch(ordenActual, (newVal) => {
-  if (newVal === null) {
-    localStorage.setItem('ordenActual', 'null'); // Guardar 'null' cuando no hay orden
-  } else {
-    localStorage.setItem('ordenActual', newVal); // Guardar 'asc' o 'desc'
-  }
-});
-
-// Ejecutar la función cuando el componente se monta
+// Ejecutar la función al montar el componente
 onMounted(() => {
-  fetchProductosHotel();
+  fetchProductos();
+  aplicarFiltrosDesdeLocalStorage();
 });
 </script>
+
 
 <style scoped>
 .filter-container {
@@ -265,4 +301,37 @@ onMounted(() => {
   font-weight: bold;
   margin: 5px 0;
 }
+
+.material-container {
+  margin-bottom: 20px;
+  margin-top: 20px;
+  text-align: left;
+}
+
+.material-options {
+  margin-top: 10px;
+}
+
+.material-option-button {
+  display: inline-block;
+  margin: 5px 0;
+  padding: 5px 10px;
+  background-color: white;
+  border: 1px solid #ddd;
+  font-size: 14px;
+  text-transform: none;
+  cursor: pointer;
+  text-align: center;
+  color: black;
+}
+
+.material-option-button:hover {
+  background-color: #f1f1f1;
+}
+
+.material-option-button.active {
+  background-color: #add8e6; /* Azul claro */
+  color: white;
+}
+
 </style>
