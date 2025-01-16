bd1e0fd25fb2d050a28dd9614be0e008faeed965
agmg16-ua <agmg16@alu.ua.es>
2025-01-16 18:32:59 +0100

    Eliminadas tabs

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
 import HomePage from '../views/HomePage.vue'
 import AboutPage from '../views/AboutPage.vue'
 import LoginPage from '../views/LoginPage.vue'
@@ -108,28 +107,6 @@ const routes: Array<RouteRecordRaw> = [
     component: AdminPage,
     name: 'panelAdmin',
     meta: { requiresAuth: true, requiresAdmin: true }
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
   }
 ]
 
